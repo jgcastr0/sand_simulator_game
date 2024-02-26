7649bf0b8483b5515f129baa5630c4779f601ec6
Juan Castro <juanyvcss@gmail.com>
2024-02-25 21:08:23 -0300

    Update sand_simulator.py

--- sand_simulator.py
+++ sand_simulator.py
@@ -19,6 +19,7 @@ screen = pygame.display.set_mode((width, height))
 # Colors
 BACKGROUND_COLOR = tuple((0, 0, 0))
 SAND_COLOR = tuple((197, 175, 128))
+WATER_COLOR = tuple((77, 200, 253))
 TEXT_COLOR = tuple((255, 255, 255))
 
 # Clock object to control FPS rate
@@ -29,9 +30,10 @@ class TheGrid:
     def __init__(self):
         self.grid = np.zeros((int(width / particle_size), int(height / particle_size)))    # Creates the grid based on the particle size
         self.sand_positions = [] # particles positions
+        self.water_positions = []
 
 
-    def sand(self, mouseX, mouseY):
+    def sand(self, mouseX, mouseY): # Adds sand particles
         gridX, gridY = int(mouseX / particle_size), int(mouseY / particle_size)
 
         if gridX >= 0 and gridX < self.grid.shape[0] and gridY >= 0 and gridY < self.grid.shape[1]:
@@ -39,48 +41,87 @@ class TheGrid:
                 self.grid[gridX][gridY] = 1
                 self.sand_positions.append([gridX * particle_size, gridY * particle_size])
 
+    
+    def water(self, mouseX, mouseY): # Adds water particles
+        gridX, gridY = int(mouseX / particle_size), int(mouseY / particle_size)
 
-    def create(self, screen):
+        if gridX >= 0 and gridX < self.grid.shape[0] and gridY >= 0 and gridY < self.grid.shape[1]:
+            if self.grid[gridX][gridY] == 0:
+                self.grid[gridX][gridY] = 2
+                self.water_positions.append([gridX * particle_size, gridY * particle_size])
+
+
+    def create(self, screen):    # Draws the particles
         for particles in self.sand_positions:
-            pygame.draw.rect(screen, SAND_COLOR, (particles[0], particles[1], particle_size, particle_size), 0)     
+            pygame.draw.rect(screen, SAND_COLOR, (particles[0], particles[1], particle_size, particle_size), 0)
+        
+        for particles in self.water_positions:
+            pygame.draw.rect(screen, WATER_COLOR, (particles[0], particles[1], particle_size, particle_size), 0)
 
 
-    def update_particles_positions(self):
-        for particles in self.sand_positions:
-            x, y = particles
+    def update_sand_positions(self): # Updates the position of sand particles
+        for particle in self.sand_positions:
+            x, y = particle
             gridX, gridY = int(x / particle_size), int(y / particle_size)
             
             # Checks if it can move downwards
             if gridY < self.grid.shape[1] - 1 and self.grid[gridX][gridY + 1] == 0:
                 self.grid[gridX][gridY] = 0
                 self.grid[gridX][gridY + 1] = 1
-                self.sand_positions.remove([x, y])
-                self.sand_positions.append([x, y + particle_size])
+                particle[:] = [x, y + particle_size]
 
             # Checks if both lower diagonals are free and chooses one to slide down
             elif gridY < self.grid.shape[1] - 1 and gridX > 0 and gridX < self.grid.shape[0] - 1 and self.grid[gridX + 1][gridY + 1] == 0 and self.grid[gridX - 1][gridY + 1] == 0:
                 direction = choice([1, -1])
                 self.grid[gridX][gridY] = 0
                 self.grid[gridX + direction][gridY + 1] = 1
-                self.sand_positions.remove([x, y])
-                self.sand_positions.append([x + (particle_size * direction), y + particle_size])
+                particle[:] = [x + (particle_size * direction), y + particle_size]
 
             # Checks if the lower right diagonal is free and slides down to it    
             elif gridY < self.grid.shape[1] - 1 and gridX > 0 and gridX < self.grid.shape[0] - 1 and self.grid[gridX + 1][gridY + 1] == 0:
                 self.grid[gridX][gridY] = 0
                 self.grid[gridX + 1][gridY + 1] = 1
-                self.sand_positions.remove([x, y])
-                self.sand_positions.append([x + particle_size, y + particle_size])
+                particle[:] = [x + particle_size, y + particle_size]
             
             # Checks if the lower left diagonal is free and slides down to it    
             elif gridY < self.grid.shape[1] - 1 and gridX > 0 and gridX < self.grid.shape[0] - 1 and self.grid[gridX -1][gridY + 1] == 0:
                 self.grid[gridX][gridY] = 0
                 self.grid[gridX - 1][gridY + 1] = 1
-                self.sand_positions.remove([x, y])
-                self.sand_positions.append([x - particle_size, y + particle_size])
+                particle[:] = [x - particle_size, y + particle_size]
 
 
-    def display_text(text):
+    def update_water_positions(self): # Updates the position of water particles
+        for particle in self.water_positions:
+            x, y = particle
+            gridX, gridY = int(x / particle_size), int(y / particle_size)
+            
+            # Checks if it can move downwards
+            if gridY < self.grid.shape[1] - 1 and self.grid[gridX][gridY + 1] == 0:
+                self.grid[gridX][gridY] = 0
+                self.grid[gridX][gridY + 1] = 2
+                particle[:] = [x, y + particle_size]
+            
+            #Checks if the water can move to both sides
+            elif gridY < self.grid.shape[1] - 1 and gridX > 0 and gridX < self.grid.shape[0] - 1 and self.grid[gridX + 1][gridY] == 0 and self.grid[gridX - 1][gridY] == 0:
+                direction = choice([1, -1])
+                self.grid[gridX][gridY] = 0
+                self.grid[gridX + direction][gridY] = 2
+                particle[:] = [x + (direction * particle_size), y]
+            
+            # Checks if the water can move to the right
+            elif gridY < self.grid.shape[1] - 1 and gridX > 0 and gridX < self.grid.shape[0] - 1 and self.grid[gridX + 1][gridY] == 0:
+                self.grid[gridX][gridY] = 0
+                self.grid[gridX + 1][gridY] = 2
+                particle[:] = [x + particle_size, y]
+
+            #Checks if the water can move to the left
+            elif gridY < self.grid.shape[1] - 1 and gridX > 0 and gridX < self.grid.shape[0] - 1 and self.grid[gridX - 1][gridY] == 0:
+                self.grid[gridX][gridY] = 0
+                self.grid[gridX - 1][gridY] = 2
+                particle[:] = [x - particle_size, y]
+
+
+    def display_text(text): # Shows instrutions on screen
         font = pygame.font.Font(None, 27)
         text1 = font.render(text, True, TEXT_COLOR)
         text_rect = text1.get_rect(center = (width // 2, 50))
@@ -96,7 +137,7 @@ def main(start_time, elapsed_time):
         xm, ym = pygame.mouse.get_pos()
 
         # Shows some informations
-        pygame.display.set_caption(f"sand Simulator  |  FPS: {int(clock.get_fps())}  |  Number of Particles: {len(universe.sand_positions)}  |  Mouse posX: [{xm}]  Mouse posY: [{ym}]")
+        pygame.display.set_caption(f"sand Simulator  |  FPS: {int(clock.get_fps())}  |  Number of Particles: {len(universe.sand_positions) + len(universe.water_positions)}  |  Mouse posX: [{xm}]  Mouse posY: [{ym}]")
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -107,6 +148,10 @@ def main(start_time, elapsed_time):
             if mouse_buttons[0]:
                 pos = pygame.mouse.get_pos()
                 universe.sand(pos[0] - pos[0] % particle_size, pos[1] - pos[1] % particle_size)
+            
+            if mouse_buttons[2]:
+                pos = pygame.mouse.get_pos()
+                universe.water(pos[0] - pos[0] % particle_size, pos[1] - pos[1] % particle_size)
                 
 
         # Fill the screen with a color
@@ -118,7 +163,8 @@ def main(start_time, elapsed_time):
             elapsed_time = time() - start_time
 
         # Updates Particle Positions
-        universe.update_particles_positions()
+        universe.update_sand_positions()
+        universe.update_water_positions()
 
         # Draws the particles
         universe.create(screen)
@@ -127,7 +173,7 @@ def main(start_time, elapsed_time):
         pygame.display.flip()
         
         # FPS rate
-        clock.tick(120) 
+        clock.tick(60) 
 
 
 if __name__ == '__main__':
